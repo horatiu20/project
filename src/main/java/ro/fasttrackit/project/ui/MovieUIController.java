213abe7bbaec19a53d9687f3e08bd56ebe2e18c4
Horatiu Pacurar <horatiupacurar20@gmail.com>
2021-08-19 18:09:54 +0300

    ninth commit

--- src/main/java/ro/fasttrackit/project/ui/MovieUIController.java
+++ src/main/java/ro/fasttrackit/project/ui/MovieUIController.java
@@ -5,10 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ro.fasttrackit.project.service.MovieService;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("movies")
